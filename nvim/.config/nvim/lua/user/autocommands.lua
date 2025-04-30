50dc846fa9ff8bd2348a62415d1246c66f6354f5
YotaOkuda <yotaai200@gmail.com>
2025-04-30 21:26:56 +0900

    add auto load func for neo-tree

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -3,3 +3,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
   command = "Neotree toggle",
 })
 ]]
+
+local manager = require "neo-tree.sources.manager"
+
+vim.api.nvim_create_autocmd({ "TermClose", "BufWritePost" }, {
+  pattern = "*",
+  callback = function()
+    if package.loaded["neo-tree.sources.git_status"] then
+      local gs = manager.get_state "git_status"
+      require("neo-tree.sources.git_status.commands").refresh(gs)
+    end
+  end,
+})
