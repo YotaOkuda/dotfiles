1d80b8b4fe18245011de361859060b39c0dc142f
YotaOkuda <yotaai200@gmail.com>
2025-04-30 16:15:29 +0900

    add some keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,15 @@
 local map = vim.keymap.set
 
+-- keymap
+map("n", "h", "b", { silent = true })
+map("n", "l", "w", { silent = true })
+map("v", "h", "b", { silent = true })
+map("v", "l", "w", { silent = true })
+map("n", "<C-u>", "<C-u>zz", { silent = true })
+map("n", "<C-d>", "<C-d>zz", { silent = true })
+map("n", "y", "yy", { silent = true })
+
+-- Terminal 用の設定
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "term://*",
   callback = function(args)
