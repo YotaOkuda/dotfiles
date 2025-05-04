15e164e424af65cabfc6465bed103cda7529ed62
YotaOkuda <yotaai200@gmail.com>
2025-05-04 13:28:18 +0900

    fix neotree refresh func

--- nvim/.config/nvim/lua/user/autocmds.lua
+++ nvim/.config/nvim/lua/user/autocmds.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 -- Neo-tree のソース管理モジュールを経由して state を取得
 local manager = require "neo-tree.sources.manager"
 -- TermClose, BufWritePost 時にneo-treeをリフレッシュ（適切なアイコンが表示される）
-vim.api.nvim_create_autocmd({ "TermClose", "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "FileChanged", "BufWritePost" }, {
   pattern = "*",
   callback = function()
     if package.loaded["neo-tree.sources.git_status"] then
@@ -25,5 +25,13 @@ vim.api.nvim_create_autocmd({ "TermClose", "BufWritePost" }, {
     end
   end,
 })
--- test
--- test
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ToggleTermToggle",
+  callback = function()
+    if package.loaded["neo-tree.sources.git_status"] then
+      local gs = manager.get_state "git_status"
+      require("neo-tree.sources.git_status.commands").refresh(gs)
+    end
+  end,
+})
