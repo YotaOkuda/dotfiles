250aa6f93250688fd9dc9f12c0828123307c9c18
OkudaYota <yotaai200@gmail.com>
2025-05-15 16:13:54 +0900

    fix win32yank

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -20,16 +20,30 @@ if vim.fn.has "mac" == 1 then
     cache_enabled = 0,
   }
 elseif vim.fn.has "wsl" == 1 then
-  vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-      ["+"] = "/mnt/c/Users/yotao/tools/win32yank/win32yank.exe -i --crlf",
-      ["*"] = "/mnt/c/Users/yotao/tools/win32yank/win32yank.exe -i --crlf",
-    },
-    paste = {
-      ["+"] = "/mnt/c/Users/yotao/tools/win32yank/win32yank.exe -o --lf",
-      ["*"] = "/mnt/c/Users/yotao/tools/win32yank/win32yank.exe -o --lf",
-    },
-    cache_enabled = 0,
+  local candidates = {
+    "/mnt/c/Users/yotao/tools/win32yank/win32yank.exe",
+    "/mnt/c/Users/yota/tools/win32yank/win32yank.exe",
   }
+  local yank
+  for _, p in ipairs(candidates) do
+    if vim.fn.executable(p) == 1 then
+      yank = p
+      break
+    end
+  end
+
+  if yank then
+    vim.g.clipboard = {
+      name = "win32yank-wsl",
+      copy = {
+        ["+"] = yank .. " -i --crlf",
+        ["*"] = yank .. " -i --crlf",
+      },
+      paste = {
+        ["+"] = yank .. " -o --lf",
+        ["*"] = yank .. " -o --lf",
+      },
+      cache_enabled = 0,
+    }
+  end
 end
