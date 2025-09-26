e331832f934cb949fc739b9321eb381d42099441
YotaOkuda <yotaai200@gmail.com>
2025-09-26 21:09:13 +0900

    update files

--- .config/nvim/lua/yota/lazy.lua
+++ .config/nvim/lua/yota/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("yota.plugins", {
+require("lazy").setup({{ import = "yota.plugins" }, { import = "yota.plugins.lsp" } },{
   checker = {
     enabled = true,
     notify = false,
