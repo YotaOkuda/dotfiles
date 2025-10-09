d8cb46db9737aacb888ebcc401179f55104afee2
YotaOkuda <yotaai200@gmail.com>
2025-10-09 16:13:58 +0900

    which-keyの各キーマップ説明を追加

--- .config/nvim/lua/yota/plugins/which-key.lua
+++ .config/nvim/lua/yota/plugins/which-key.lua
@@ -1,13 +1,32 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		icons = {
+			-- group = "",
+		},
+	},
+
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
+		wk.add({
+			{ "<leader>f", group = "5 Fuzzy Find" },
+			{ "<leader>g", group = "6 Tmux-Navigator" },
+			{ "<leader>h", group = "10 Hunk" },
+			{ "<leader>n", group = "4 Neotree" },
+			{ "<leader>s", group = "5 Split Window" },
+			{ "<leader>t", group = "5 Tab" },
+			{ "<leader>w", group = "2 Auto-session" },
+			{ "<leader>x", group = "5 Trouble" },
+		})
+	end,
 }
