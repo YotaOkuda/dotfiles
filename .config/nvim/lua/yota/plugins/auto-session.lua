0d2333c75cffbd9de78f2d5b04e9ef65b03262c6
YotaOkuda <yotaai200@gmail.com>
2025-10-13 19:34:14 +0900

    復元した時にウィンドウ幅が均等になるように調整

--- .config/nvim/lua/yota/plugins/auto-session.lua
+++ .config/nvim/lua/yota/plugins/auto-session.lua
@@ -8,9 +8,21 @@ return {
 			auto_restore_enabled = false,
 			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 
-			post_restore_cmds = {
-				"Neotree toggle left",
-			},
+			-- post_restore_cmds = {
+			-- 	"Neotree toggle left",
+			-- },
+		})
+		vim.api.nvim_create_autocmd("SessionLoadPost", {
+			callback = function()
+				-- セッション復元が終わったあと、0.1秒遅らせて処理
+				vim.defer_fn(function()
+					-- Neo-treeを左に開く
+					vim.cmd("Neotree show left")
+
+					-- すべてのウィンドウを均等にする（Neo-treeはwidth=35固定なので右側が整う）
+					vim.cmd("wincmd =")
+				end, 100) -- ← 100ミリ秒後に実行
+			end,
 		})
 
 		local keymap = vim.keymap
