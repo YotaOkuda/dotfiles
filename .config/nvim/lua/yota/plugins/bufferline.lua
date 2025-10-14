4ef532c52e16a8893e534920058e04a94f56b8df
YotaOkuda <yotaai200@gmail.com>
2025-10-14 15:24:59 +0900

    bufferlineウィンドウを消すコマンドを追加

--- .config/nvim/lua/yota/plugins/bufferline.lua
+++ .config/nvim/lua/yota/plugins/bufferline.lua
@@ -8,6 +8,7 @@ return {
 	vim.keymap.set("n", "<S-h>", ":BufferLineCyclePrev<CR>", { silent = true }),
 	vim.keymap.set("n", "<leader>bl", ":BufferLineMoveNext<CR>", { silent = true, desc = "Move to next buffer" }),
 	vim.keymap.set("n", "<leader>bh", ":BufferLineMovePrev<CR>", { silent = true, desc = "Move to previous buffer" }),
+	vim.keymap.set("n", "<leader>bw", ":bdelete<CR>", { silent = true, desc = "Close current buffer" }),
 
 	opts = {
 		options = {
