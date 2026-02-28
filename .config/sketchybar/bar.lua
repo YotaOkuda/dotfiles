d2346dd18e6c8e4e96efabee6fcf336cf25db369
YotaOkuda <yotaai200@gmail.com>
2026-02-28 19:29:11 +0900

    高さ調整

--- .config/sketchybar/bar.lua
+++ .config/sketchybar/bar.lua
@@ -3,7 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	topmost = "window",
-	height = 32,
+	height = 35,
 	-- color = colors.black,
 	color = colors.transparent,
 	padding_right = 2,
